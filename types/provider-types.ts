954723da258870f6f36a6bb2180aa5eacc143756
sean-9875134532495897234523 <95354111+sean-9875134532495897234523@users.noreply.github.com>
2021-12-09 12:52:17 +0000

    Implement provider types for ddcms

--- types/provider-types.ts
+++ types/provider-types.ts
@@ -1,13 +1,19 @@
 import { Address, AccountDetails } from './component-types.ts';
 
 export interface AdditionalCustomerFields {
-
+    accountDetails: AccountDetails;
+    address: Address;
+    emailAddress: string;
+    customerRef: string; 
+    title: string;
 }
 
 export interface ProviderCustomerIdentifier {
-
+    id: string;
+    customerRef: string; 
 }
 
 export interface Merchant {
-
+    name: string;
+    clientCode: string;
 }
