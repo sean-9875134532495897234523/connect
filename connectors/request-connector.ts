2e73d457a34fd276fa113c2518fadcc67269fe07
sean-9875134532495897234523 <95354111+sean-9875134532495897234523@users.noreply.github.com>
2021-12-09 12:50:38 +0000

    Implement request connector example for ddcms

--- connectors/request-connector.ts
+++ connectors/request-connector.ts
@@ -11,11 +11,42 @@ import {
 export class RequestConnector extends BaseConnector implements IRequestConnector {
 
     onCreateCustomer(customer: Customer): HttpRequest {
-        throw new Error("Method not implemented.");
+
+        const data: Record<string, unknown> = {
+            "Email": customer.emailAddress,
+            "Title": customer.title,
+            "CustomerRef": customer.customerRef,
+            "FirstName": customer.firstName,
+            "Surname": customer.lastName,
+            "Line1": customer.address.line1,
+            "Line2": customer.address.line2,
+            "PostCode": customer.address.postcode,
+            "AccountNumber": customer.accountDetails.accountNumber,
+            "BankSortCode": customer.accountDetails.sortCode,
+            "AccountHolderName": customer.accountDetails.accountName
+        };
+
+        const req: HttpRequest = {
+            method: HttpRequestMethod.POST,
+            uri: new URL(this._connect.PROVIDER.BASE_URI +
+                `/client/${customer.merchant.clientCode}/customer`),
+            accept: "application/json",
+            data: data
+        };
+
+        return req;
     }
-    
-    onCreateCustomerSuccess(response: HttpResponse): ProviderCustomerIdentifier {
-        throw new Error("Method not implemented.");
+
+    onCreateCustomerSuccess(response: HttpResponse) : ProviderCustomerIdentifier {
+
+        const identifiers : ProviderCustomerIdentifier = {
+            id: typeof response.data.Id === 'string' ?
+                response.data.Id : null as unknown as string,
+            customerRef: typeof response.data.CustomerRef === 'string' ?
+                response.data.CustomerRef : null as unknown as string,
+        };
+        
+        return identifiers;
     }
 
 }
